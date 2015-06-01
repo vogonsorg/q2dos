8052b6b07e00a780257feac01f3c07a15e82905b
maraakate <emoaddict15@gmail.com>
2015-06-01 14:49:38 -0400

    sys_find functions working, mkdir works, but breaks dosbox

--- dos/sys_dos.c
+++ dos/sys_dos.c
@@ -18,6 +18,7 @@
 #include "../qcommon/qcommon.h"
 #include "../client/keys.h"
 #include "errno.h"
+#include "glob.h"
 
 int _crt0_startup_flags = _CRT0_FLAG_UNIX_SBRK; // FS: Fake Mem Fix (QIP)
 #define KEYBUF_SIZE     256
@@ -328,23 +329,98 @@ int		Sys_Milliseconds (void)
 
 void	Sys_Mkdir (char *path)
 {
-//	mkdir (path, 0777);
-	printf("Sys_Mkdir [%s]: UNIMPLEMENTED!\n",path);
+	// FS: This works, just not on DOSBOX for some reason.
+	mkdir (path, 0777);
+//	printf("Sys_Mkdir [%s]: UNIMPLEMENTED!\n",path);
+}
+static	DIR		*fdir;
+static	char	findbase[MAX_OSPATH];
+static	char	findpath[MAX_OSPATH];
+static	char	findpattern[MAX_OSPATH];
+
+static qboolean CompareAttributes(char *path, char *name,
+	unsigned musthave, unsigned canthave )
+{
+	struct stat st;
+	char fn[MAX_OSPATH];
+
+// . and .. never match
+	if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
+		return false;
+
+	return true;
+
+	if (stat(fn, &st) == -1)
+		return false; // shouldn't happen
+
+	if ( ( st.st_mode & S_IFDIR ) && ( canthave & SFF_SUBDIR ) )
+		return false;
+
+	if ( ( musthave & SFF_SUBDIR ) && !( st.st_mode & S_IFDIR ) )
+		return false;
+
+	return true;
 }
 
-char	*Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
+char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
 {
-	printf("Sys_FindFirst [%s]: UNIMPLEMENTED!\n",path);
+	struct dirent *d;
+	char *p;
+
+	if (fdir)
+		Sys_Error ("Sys_BeginFind without close");
+
+//	COM_FilePath (path, findbase);
+	strcpy(findbase, path);
+
+	if ((p = strrchr(findbase, '/')) != NULL) {
+		*p = 0;
+		strcpy(findpattern, p + 1);
+	} else
+		strcpy(findpattern, "*");
+
+	if (strcmp(findpattern, "*.*") == 0)
+		strcpy(findpattern, "*");
+	
+	if ((fdir = opendir(findbase)) == NULL)
+		return NULL;
+	while ((d = readdir(fdir)) != NULL) {
+		if (!*findpattern || glob_match(findpattern, d->d_name)) {
+//			if (*findpattern)
+//				printf("%s matched %s\n", findpattern, d->d_name);
+			if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+				sprintf (findpath, "%s/%s", findbase, d->d_name);
+				return findpath;
+			}
+		}
+	}
 	return NULL;
 }
 
-char	*Sys_FindNext (unsigned musthave, unsigned canthave)
+char *Sys_FindNext (unsigned musthave, unsigned canhave)
 {
+	struct dirent *d;
+
+	if (fdir == NULL)
+		return NULL;
+	while ((d = readdir(fdir)) != NULL) {
+		if (!*findpattern || glob_match(findpattern, d->d_name)) {
+//			if (*findpattern)
+//				printf("%s matched %s\n", findpattern, d->d_name);
+			if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+				sprintf (findpath, "%s/%s", findbase, d->d_name);
+				return findpath;
+			}
+		}
+	}
 	return NULL;
 }
 
-void	Sys_FindClose (void)
+void Sys_FindClose (void)
 {
+	if (fdir != NULL)
+		closedir(fdir);
+	fdir = NULL;
 }
 
 void	Sys_Init (void)
