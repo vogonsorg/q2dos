14dea76413321457b2df6fb6cae7ac264a462edf
maraakate <emoaddict15@gmail.com>
2015-06-02 23:46:48 -0400

    cd audio works, volume always on though, need to fix

--- dos/cd_dos.c
+++ dos/cd_dos.c
@@ -62,7 +62,7 @@ void CDAudio_Shutdown(void)
 
 
 int realtime;
-cvar_t	*bgmvolume;
+//cvar_t	*bgmvolume;
 
 #define ADDRESS_MODE_HSG		0
 #define ADDRESS_MODE_RED_BOOK	1
@@ -565,15 +565,15 @@ void CDAudio_Play(int track, qboolean looping)
 		return;
 	}
 
-	volume = (int)(bgmvolume->value * 255.0);
+	volume = 255; //(int)(bgmvolume->value * 255.0);
 	if (volume < 0)
 	{
-		Cvar_SetValue ("bgmvolume", 0.0);
+//		Cvar_SetValue ("bgmvolume", 0.0);
 		volume = 0;
 	}
 	else if (volume > 255)
 	{
-		Cvar_SetValue ("bgmvolume", 1.0);
+//		Cvar_SetValue ("bgmvolume", 1.0);
 		volume = 255;
 	}
 	CDAudio_SetVolume (volume);
@@ -808,17 +808,17 @@ void CDAudio_Update(void)
 		}
 	}
 
-	newVolume = (int)(bgmvolume->value * 255.0);
+	newVolume = 255; //(int)(bgmvolume->value * 255.0);
 	if (newVolume != cdvolume)
 	{
 		if (newVolume < 0)
 		{
-			Cvar_SetValue ("bgmvolume", 0.0);
+//			Cvar_SetValue ("bgmvolume", 0.0);
 			newVolume = 0;
 		}
 		else if (newVolume > 255)
 		{
-			Cvar_SetValue ("bgmvolume", 1.0);
+//			Cvar_SetValue ("bgmvolume", 1.0);
 			newVolume = 255;
 		}
 		CDAudio_SetVolume (newVolume);
@@ -842,7 +842,7 @@ int CDAudio_Init(void)
 	char	*memory;
 	int		n;
 
-	cvar_t *bgmvolume = Cvar_Get ("cd_volume", "1.0", 0);
+//	cvar_t *bgmvolume = Cvar_Get ("cd_volume", "1.0", 0);
 /*	if (cls.state == ca_dedicated) // FS: FIXME
 		return -1;
 */
