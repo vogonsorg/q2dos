d6dce43ac9d417d39b9b55f6f5e80ecaf20a1dbc
sezero <sezero@users.sourceforge.net>
2015-06-29 20:15:17 +0300

    ref_soft/r_model.c (Mod_LoadTexinfo): fix undefined behavior [-Waggressive-loop-optimizations]

--- ref_soft/r_model.c
+++ ref_soft/r_model.c
@@ -492,9 +492,10 @@ void Mod_LoadTexinfo (lump_t *l)
 
 	for ( i=0 ; i<count ; i++, in++, out++)
 	{
-		for (j=0 ; j<8 ; j++)
+		for (j=0 ; j<4 ; j++)
 		{
 			out->vecs[0][j] = LittleFloat (in->vecs[0][j]);
+			out->vecs[1][j] = LittleFloat (in->vecs[1][j]);
 		}
 		len1 = VectorLength (out->vecs[0]);
 		len2 = VectorLength (out->vecs[1]);
