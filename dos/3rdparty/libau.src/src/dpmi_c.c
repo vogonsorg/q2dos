e7a909df463cc35279595d714d3f0c1ed0629a42
maraakate <emoaddict15@gmail.com>
2015-07-02 13:00:25 -0400

    very small clean up tweak to the if statements in dpmi_c.c

--- dos/3rdparty/libau.src/src/dpmi_c.c
+++ dos/3rdparty/libau.src/src/dpmi_c.c
@@ -1,6 +1,7 @@
 #include "libaudef.h"
 
 struct dosmem_t au_dosmem;
+
 #ifdef ZDM
 unsigned short au_map_selector;
 #endif
@@ -9,6 +10,7 @@ void pds_dpmi_dos_freemem(void)
 {
 	struct dosmem_t *dm = &au_dosmem;
 	unsigned short sel = dm->selector;
+
 #ifdef __DJGPP__
 	__asm__("movw %0,%%dx"::"m"(sel));
 	__asm__("movw $0x101,%ax");
@@ -31,7 +33,10 @@ struct dosmem_t *pds_dpmi_dos_allocmem(unsigned int size)
 
 #ifdef __DJGPP__
   #ifndef ZDM
-	if(!__djgpp_nearptr_enable()) return NULL;
+	if(!__djgpp_nearptr_enable())
+	{
+		return NULL;
+	}
   #endif
 
 	__asm__("movl %0,%%ebx"::"m"(size));
@@ -75,27 +80,42 @@ unsigned long pds_dpmi_map_physical_memory(unsigned long phys_addr,unsigned long
 	__dpmi_meminfo mi;
 	mi.address = phys_addr;
 	mi.size = memsize;
-	if(__dpmi_physical_address_mapping(&mi) != 0) return 0;
+
+	if(__dpmi_physical_address_mapping(&mi) != 0)
+	{
+		return 0;
+	}
+
 	au_map_selector = __dpmi_allocate_ldt_descriptors(1);
 	__dpmi_set_segment_base_address (au_map_selector,mi.address);
 	__dpmi_set_segment_limit (au_map_selector, mi.size - 1);
+
 	return mi.address;
 
 #else
 	unsigned long ret;
 	union REGS regs;
+
 	memset(&regs,0,sizeof(union REGS));
+
 	regs.w.ax = 0x0800;
 	regs.w.bx = (phys_addr>>16);
 	regs.w.cx = (phys_addr&0xffff);
 	regs.w.di = (memsize&0xffff);
 	regs.w.si = (memsize>>16);
 	int386(0x31,&regs,&regs);
-	if(regs.w.cflag) return 0;
+
+	if(regs.w.cflag)
+	{
+		return 0;
+	}
+
 	ret = ((unsigned long)regs.w.bx<<16) | (regs.w.cx&0xffff);
+
 	#ifdef __DJGPP__
-	ret += __djgpp_conventional_base;
+		ret += __djgpp_conventional_base;
 	#endif
+
 	return ret;
 #endif
 }
@@ -109,7 +129,9 @@ void pds_dpmi_unmap_physycal_memory(unsigned long linear_addr)
 
 #else
 	union REGS regs;
+
 	memset(&regs,0,sizeof(union REGS));
+
 	regs.w.ax = 0x0801;
 	regs.w.bx = (linear_addr>>16);
 	regs.w.cx = (linear_addr&0xffff);
