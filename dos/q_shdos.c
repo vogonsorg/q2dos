vogonsorg/q2dos

commit 1fd768838e21a8f0c0dac54fa294f3b126c8d1f8

lots of small clean-ups, cleaner+smaller exe

dos/q_shdos.c
@@ -3,20 +3,12 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <stdio.h>
+#include <dos.h>
 #include <dirent.h>
-#include <unistd.h>
-#include <sys/mman.h>
+#include <io.h>
 #include <sys/time.h>
-#include <sys/nearptr.h>
-#include <dpmi.h>
-#include <conio.h>
-#include <bios.h>
-#include <crt0.h> // FS: Fake Mem Fix (QIP)
 
-#include "dosisms.h"
 #include "../qcommon/qcommon.h"
-#include "../client/keys.h"
-#include "errno.h"
 #include "glob.h"
 
 static byte	*membase;
@@ -25,33 +17,25 @@ static int	curhunksize;
 
 void	*Hunk_Begin (int maxsize)
 {
-	// reserve a huge chunk of memory, but don't commit any yet
+	/* reserve a huge chunk of memory, but don't commit any yet */
 	maxhunksize = maxsize;
 	curhunksize = 0;
 	membase = malloc (maxhunksize);
-
 	if (!membase)
-	{
 		Sys_Error ("VirtualAlloc reserve failed %d bytes",maxsize);
-	}
 
 	memset (membase, 0, maxsize);
 	return (void *)membase;
-
 }
 
 void	*Hunk_Alloc (int size)
 {
-//	void	*buf;
-
-	// round to cacheline
+	/* round to cacheline */
 	size = (size+31)&~31;
 
 	curhunksize += size;
 	if (curhunksize > maxhunksize)
-	{
 		Sys_Error ("Hunk_Alloc overflow");
-	}
 
 	return (void *)(membase+curhunksize-size);
 }
@@ -66,7 +50,7 @@ int	Hunk_End (void)
 	byte *n = realloc(membase, curhunksize);
 	if (n != membase)
 		Sys_Error("Hunk_End:  Could not remap virtual block (%d)", errno);
-	
+
 	return curhunksize;
 }
 
@@ -83,11 +67,10 @@ int	Sys_LinuxTime (void) // FS: DOS needs this for random qport
 {
 	int linuxtime;
 	struct timeval tp;
-	struct timezone tzp;
 	static int		secbase;
 
-	gettimeofday(&tp, &tzp);
-	
+	gettimeofday(&tp, NULL);
+
 	if (!secbase)
 	{
 		secbase = tp.tv_sec;
@@ -95,7 +78,7 @@ int	Sys_LinuxTime (void) // FS: DOS needs this for random qport
 	}
 
 	linuxtime = (tp.tv_sec - secbase)*1000 + tp.tv_usec/1000;
-	
+
 	return linuxtime;
 }
 
@@ -110,30 +93,31 @@ static	char	findpath[MAX_OSPATH];
 static	char	findpattern[MAX_OSPATH];
 
 static qboolean CompareAttributes(char *path, char *name,
-	unsigned musthave, unsigned canthave )
+				  unsigned musthave, unsigned canthave)
 {
-	struct stat st;
+	int attr;
 	char fn[MAX_OSPATH];
 
-// . and .. never match
+	/* . and .. never match */
 	if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
 		return false;
 
 	return true;
 
-	if (stat(fn, &st) == -1)
-		return false; // shouldn't happen
-
-	if ( ( st.st_mode & S_IFDIR ) && ( canthave & SFF_SUBDIR ) )
+	sprintf (fn, "%s/%s", path, name);
+	if ((attr = _chmod(path, 0)) == -1) /* shouldn't happen */
 		return false;
 
-	if ( ( musthave & SFF_SUBDIR ) && !( st.st_mode & S_IFDIR ) )
+	if ((attr & _A_SUBDIR) && (canthave & SFF_SUBDIR))
+		return false;
+
+	if ((musthave & SFF_SUBDIR) && !(attr & _A_SUBDIR))
 		return false;
 
 	return true;
 }
 
-char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
+char *Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
 {
 	struct dirent *d;
 	char *p;
@@ -159,7 +143,7 @@ char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
 		if (!*findpattern || glob_match(findpattern, d->d_name)) {
 //			if (*findpattern)
 //				printf("%s matched %s\n", findpattern, d->d_name);
-			if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+			if (CompareAttributes(findbase, d->d_name, musthave, canthave)) {
 				sprintf (findpath, "%s/%s", findbase, d->d_name);
 				return findpath;
 			}
@@ -168,7 +152,7 @@ char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
 	return NULL;
 }
 
-char *Sys_FindNext (unsigned musthave, unsigned canhave)
+char *Sys_FindNext (unsigned musthave, unsigned canthave)
 {
 	struct dirent *d;
 
@@ -178,7 +162,7 @@ char *Sys_FindNext (unsigned musthave, unsigned canhave)
 		if (!*findpattern || glob_match(findpattern, d->d_name)) {
 //			if (*findpattern)
 //				printf("%s matched %s\n", findpattern, d->d_name);
-			if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+			if (CompareAttributes(findbase, d->d_name, musthave, canthave)) {
 				sprintf (findpath, "%s/%s", findbase, d->d_name);
 				return findpath;
 			}

client/menu.c
@@ -30,9 +30,9 @@ int maxclients;
 #endif
 
 #ifdef __DJGPP__
-	extern int	havegus; // FS: DOS GUS
+extern int	havegus; /* FS: DOS GUS */
 #else
-	int havegus = 0;
+static const int havegus = 0;
 #endif
 
 static int	m_main_cursor;

dos/cd_dos.c
@@ -27,36 +27,27 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../client/client.h"
 
 #ifndef USE_DOS_CD
-void CDAudio_Activate (qboolean active)
-{
-}
-
 void CDAudio_Play(int track, qboolean looping)
 {
 }
-
 
 void CDAudio_Stop(void)
 {
 }
 
-
 void CDAudio_Resume(void)
 {
 }
 
-
 void CDAudio_Update(void)
 {
 }
-
 
 int CDAudio_Init(void)
 {
 	Com_Printf("CD Audio is disabled in this build.\n");
 	return 0;
 }
-
 
 void CDAudio_Shutdown(void)
 {
@@ -380,7 +371,7 @@ static int CDAudio_GetAudioTrackInfo (byte track, int *start)
 
 	if (cdRequest->status & STATUS_ERROR_BIT)
 	{
-		Com_DPrintf(DEVELOPER_MSG_CD, "CDAudio_GetAudioTrackInfo %04x\n", cdRequest->status & 	0xffff);
+		Com_DPrintf(DEVELOPER_MSG_CD, "CDAudio_GetAudioTrackInfo %04x\n", cdRequest->status & 0xffff);
 		return -1;
 	}
 
@@ -416,7 +407,7 @@ static int CDAudio_GetAudioDiskInfo (void)
 
 	if (cdRequest->status & STATUS_ERROR_BIT)
 	{
-		Com_DPrintf(DEVELOPER_MSG_CD, "CDAudio_GetAudioDiskInfo %04x\n", cdRequest->status & 	0xffff);
+		Com_DPrintf(DEVELOPER_MSG_CD, "CDAudio_GetAudioDiskInfo %04x\n", cdRequest->status & 0xffff);
 		return -1;
 	}
 
@@ -496,7 +487,7 @@ static int CDAudio_MediaChange (void)
 
 // we set the volume to 0 first and then to the desired volume
 // some cd-rom drivers seem to need it done this way
-void CDAudio_SetVolume (byte volume)
+static void CDAudio_SetVolume (byte volume)
 {
 	if (!initialized || !enabled)
 		return;
@@ -917,12 +908,7 @@ int CDAudio_Init (void)
 	dos_int86 (0x2f);
 	if (regs.x.bx == 0)
 	{
-		Com_Printf(
-			"MSCDEX not loaded, CD Audio is\n"
-			"disabled.  Use \"-nocdaudio\" if you\n"
-			"wish to avoid this message in the\n"
-			"future.  See README.TXT for help.\n"
-			);
+		Com_Printf ("MSCDEX not loaded, CD Audio is disabled.\n");
 		return -1;
 	}
 
@@ -935,9 +921,7 @@ int CDAudio_Init (void)
 	dos_int86 (0x2f);
 	if (regs.x.bx == 0)
 	{
-		Com_Printf ("MSCDEX version 2.00 or later\n"
-				"required for CD Audio.\n"
-				"See README.TXT for help.\n");
+		Com_Printf("%s: MSCDEX version 2.00 or later required.\n", __FUNCTION__);
 		return -1;
 	}
 
@@ -966,7 +950,7 @@ int CDAudio_Init (void)
 	memory = (char *) dos_getmemory(sizeof(struct cd_request) + sizeof(union readInfo_u));
 	if (memory == NULL)
 	{
-		Com_DPrintf(DEVELOPER_MSG_CD, "CDAudio_Init: Unable to allocate low memory.\n");
+		Com_DPrintf(DEVELOPER_MSG_CD, "%s: Unable to allocate low memory.\n", __FUNCTION__);
 		return -1;
 	}
 
@@ -997,7 +981,7 @@ int CDAudio_Init (void)
 }
 
 
-void CDAudio_Shutdown(void)
+void CDAudio_Shutdown (void)
 {
 	if (!initialized)
 		return;
@@ -1015,4 +999,4 @@ qboolean CDAudio_Active (void)
 	return playing;
 }
 
-#endif // USE_DOS_CD
+#endif /* USE_DOS_CD */

dos/snd_gus.c
@@ -33,9 +33,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define INI_STRING_SIZE 0x100
 
-FILE *ini_fopen(const char *filename, const char *modes);
-int ini_fclose(FILE *f);
-void ini_fgets(FILE *f, const char *section, const char *field, char *s);
+static FILE *ini_fopen(const char *filename, const char *modes);
+static int ini_fclose(FILE *f);
+static void ini_fgets(FILE *f, const char *section, const char *field, char *s);
 
 // Routines for reading from .INI files
 // The read routines are fairly efficient.
@@ -61,7 +61,7 @@ struct field_buffer
 	char name[MAX_FIELD_WIDTH+1];
 };
 
-static FILE *current_file=NULL;
+static FILE *current_file = NULL;
 static int	current_section;
 
 static int current_section_buffer=0;
@@ -69,7 +69,7 @@ static int current_field_buffer=0;
 
 static struct section_buffer section_buffers[NUM_SECTION_BUFFERS];
 static struct field_buffer field_buffers[NUM_FIELD_BUFFERS];
-byte	extVoices,extCodecVoices; // FS: GUS clicking sounds during map transitions and pauseing fix
+static byte	extVoices,extCodecVoices; // FS: GUS clicking sounds during map transitions and pauseing fix
 int	havegus; // FS
 
 //***************************************************************************
@@ -335,17 +335,14 @@ static char *stripped_fgets(char *s, int n, FILE *f)
 	return(s);
 }
 
-//***************************************************************************
-// Externally accessable routines
-//***************************************************************************
 // Opens an .INI file. Works like fopen
-FILE *ini_fopen(const char *filename, const char *modes)
+static FILE *ini_fopen(const char *filename, const char *modes)
 {
 	return(fopen(filename,modes));
 }
 
 // Closes a .INI file. Works like fclose
-int ini_fclose(FILE *f)
+static int ini_fclose(FILE *f)
 {
 	if (f==current_file)
 		reset_buffer(NULL);
@@ -355,7 +352,7 @@ int ini_fclose(FILE *f)
 // Puts "field" from "section" from .ini file "f" into "s".
 // If "section" does not exist or "field" does not exist in
 // section then s="";
-void ini_fgets(FILE *f, const char *section, const char *field, char *s)
+static void ini_fgets(FILE *f, const char *section, const char *field, char *s)
 {
 	int i;
 	long start_pos,string_start_pos;
@@ -604,31 +601,31 @@ static struct Gf1RateStruct Gf1Rates[]=
 //=============================================================================
 // Basic GF1 functions
 //=============================================================================
-void SetGf18(BYTE reg,BYTE data)
+static void SetGf18(BYTE reg,BYTE data)
 {
 	dos_outportb(Gf1RegisterSelect,reg);
 	dos_outportb(Gf1DataHigh,data);
 }
 
-void SetGf116(BYTE reg,WORD data)
+static void SetGf116(BYTE reg,WORD data)
 {
 	dos_outportb(Gf1RegisterSelect,reg);
 	dos_outportw(Gf1DataLow,data);
 }
 
-BYTE GetGf18(BYTE reg)
+static BYTE GetGf18(BYTE reg)
 {
 	dos_outportb(Gf1RegisterSelect,reg);
 	return(dos_inportb(Gf1DataHigh));
 }
 
-WORD GetGf116(BYTE reg)
+static WORD GetGf116(BYTE reg)
 {
 	dos_outportb(Gf1RegisterSelect,reg);
 	return(dos_inportw(Gf1DataLow));
 }
 
-void Gf1Delay(void)
+static void Gf1Delay(void)
 {
 	int i;
 
@@ -636,12 +633,12 @@ void Gf1Delay(void)
 		dos_inportb(Gf1TimerControl);
 }
 
-DWORD ConvertTo16(DWORD Address)
+static DWORD ConvertTo16(DWORD Address)
 {
 	return( ((Address>>1) & 0x0001FFFF) | (Address & 0x000C0000L) );
 }
 
-void ClearGf1Ints(void)
+static void ClearGf1Ints(void)
 {
 	int i;
 
@@ -1266,26 +1263,25 @@ int GUS_GetDMAPos(void)
 	if (HaveCodec)
 	{
 		// clear 16-bit reg flip-flop
- 	  // load the current dma count register
- 	  if (DmaChannel < 4)
- 	  {
- 		  dos_outportb(0x0C, 0);
- 		  count = dos_inportb(CountReg);
- 		  count += dos_inportb(CountReg) << 8;
- 		  if (dma.samplebits == 16)
- 			  count /= 2;
- 		  count = dma.samples - (count+1);
- 	  }
- 	  else
- 	  {
- 		  dos_outportb(0xD8, 0);
- 		  count = dos_inportb(CountReg);
- 		  count += dos_inportb(CountReg) << 8;
- 		  if (dma.samplebits == 8)
- 			  count *= 2;
- 		  count = dma.samples - (count+1);
- 	  }
-
+		// load the current dma count register
+		if (DmaChannel < 4)
+		{
+			dos_outportb(0x0C, 0);
+			count = dos_inportb(CountReg);
+			count += dos_inportb(CountReg) << 8;
+			if (dma.samplebits == 16)
+				count /= 2;
+			count = dma.samples - (count+1);
+		}
+		else
+		{
+			dos_outportb(0xD8, 0);
+			count = dos_inportb(CountReg);
+			count += dos_inportb(CountReg) << 8;
+			if (dma.samplebits == 8)
+				count *= 2;
+			count = dma.samples - (count+1);
+		}
 	}
 	else
 	{
@@ -1343,6 +1339,7 @@ void GUS_Shutdown (void)
 	dos_freememory (dma_dosadr);
 	dma_dosadr = NULL; // sezero
 }
+
 void GUS_ClearDMA (void) // FS: This stops the constant clicking sound during map loads and pause screens
 {
 	memset(dma_buffer, 0, SND_BUFFER_SIZE);
@@ -1386,5 +1383,5 @@ void GUS_ClearDMA (void) // FS: This stops the constant clicking sound during ma
 			SetGf18(DMA_CONTROL,0x45);
 		GUS_StartGf1(SND_BUFFER_SIZE,extVoices);
 	}
-        Com_DPrintf(DEVELOPER_MSG_SOUND, "Cleared GUS DMA Buffer!\n");
+	Com_DPrintf(DEVELOPER_MSG_SOUND, "Cleared GUS DMA Buffer!\n");
 }

dos/snddma_dos.c
@@ -18,8 +18,8 @@ typedef enum
 	dma_gus
 } dmacard_t;
 
-dmacard_t		 dmacard;
-int BLASTER_GetDMAPos(void);
+static dmacard_t		 dmacard;
+static int BLASTER_GetDMAPos(void);
 
 /*
 ===============================================================================
@@ -54,12 +54,13 @@ static  int high_dma;
 static  int mixer_port;
 static  int mpu401_port;
 
-int	dsp_version;
-int	dsp_minor_version;
+static int	dsp_version;
+static int	dsp_minor_version;
 
-int	timeconstant=-1;
+static int	timeconstant=-1;
 
-void PrintBits (byte b)
+#if 0
+static void PrintBits (byte b)
 {
 	int		i;
 	char	str[9];
@@ -70,8 +71,9 @@ void PrintBits (byte b)
 	str[8] = 0;
 	Com_Printf ("%s (%i)", str, b);
 }
+#endif
 
-void SB_Info_f(void)
+static void SB_Info_f(void)
 {
 	Com_Printf ("BLASTER=%s\n", getenv("BLASTER"));
 	Com_Printf("dsp version=%d.%d\n", dsp_version, dsp_minor_version);
@@ -85,7 +87,7 @@ void SB_Info_f(void)
 // Interprets BLASTER variable
 // =======================================================================
 
-int GetBLASTER(void)
+static int GetBLASTER(void)
 {
 	char *BLASTER;
 	char *param;
@@ -143,7 +145,7 @@ int GetBLASTER(void)
 // Resets DSP.  Returns 0 on success.
 // ==================================================================
 
-int ResetDSP(void)
+static int ResetDSP(void)
 {
 	volatile int i;
 
@@ -157,34 +159,33 @@ int ResetDSP(void)
 	}
 	if (i) return 0;
 	else return 1;
-
 }
 
-int ReadDSP(void)
+static int ReadDSP(void)
 {
 	while (!(dos_inportb(dsp_port+0xe)&0x80)) ;
 	return dos_inportb(dsp_port+0xa);
 }
 
-void WriteDSP(int val)
+static void WriteDSP(int val)
 {
 	while ((dos_inportb(dsp_port+0xc)&0x80)) ;
 	dos_outportb(dsp_port+0xc, val);
 }
 
-int ReadMixer(int addr)
+static int ReadMixer(int addr)
 {
 	dos_outportb(mixer_port+4, addr);
 	return dos_inportb(mixer_port+5);
 }
 
-void WriteMixer(int addr, int val)
+static void WriteMixer(int addr, int val)
 {
 	dos_outportb(mixer_port+4, addr);
 	dos_outportb(mixer_port+5, val);
 }
 
-int				 oldmixervalue;
+static int	oldmixervalue;
 
 /*
 ================
@@ -192,7 +193,7 @@ StartSB
 
 ================
 */
-void StartSB(void)
+static void StartSB(void)
 {
 	int	i;
 
@@ -273,7 +274,7 @@ StartDMA
 
 ================
 */
-void StartDMA(void)
+static void StartDMA(void)
 {
 	int mode;
 	int realaddr;
@@ -367,11 +368,8 @@ qboolean BLASTER_Init(void)
 //
 	if (!GetBLASTER())
 	{
-		Com_Printf (
-		"The BLASTER environment variable\n"
-		"is not set, sound effects are\n"
-		"disabled.  See README.TXT for help.\n"
-		);							 
+		Com_Printf ("The BLASTER environment variable is not set,\n"
+				"Sound Blaster support is disabled.\n");
 		return 0;
 	}
 
@@ -563,8 +561,7 @@ Returns false if nothing is found.
 */
 qboolean PCI_Init(void)
 {
-struct mpxplay_audioout_info_s *aui=&au_infos;
-
+	struct mpxplay_audioout_info_s *aui=&au_infos;
 	const char *c;
 
 	c = AU_search(1);/* 1: stereo speaker output (meaningful only for Intel HDA chips) */
@@ -585,7 +582,7 @@ struct mpxplay_audioout_info_s *aui=&au_infos;
 		dma.channels=channels;
 		AU_setrate(&speed,&samplebits,&channels);
 		
-#if 1		//not sure but I think it can change in the setrate
+#if 1		/* not sure but I think it can change in the setrate */
 		dma.speed=aui->freq_card;
 		dma.samplebits=aui->bits_set;
 		dma.channels=aui->chan_set;
@@ -754,5 +751,5 @@ void SNDDMA_Submit(void)
 
 void S_Activate (qboolean active)
 {
-	Com_Printf("S_Activate %d", active);
+	Com_Printf("S_Activate %d\n", active);
 }

dos/sys_dos.c
@@ -25,21 +25,19 @@ int _crt0_startup_flags = _CRT0_FLAG_UNIX_SBRK; // FS: Fake Mem Fix (QIP)
 #include "dosisms.h"
 #include "../qcommon/qcommon.h"
 #include "../client/keys.h"
-#include "errno.h"
 #include "glob.h"
 
-#define MINIMUM_WIN_MEMORY                      0x800000
-#define MINIMUM_WIN_MEMORY_LEVELPAK     (MINIMUM_WIN_MEMORY + 0x100000)
+#define MINIMUM_WIN_MEMORY		0x800000
+#define MINIMUM_WIN_MEMORY_LEVELPAK	(MINIMUM_WIN_MEMORY + 0x100000)
 
-#define KEYBUF_SIZE     256
-static unsigned char    keybuf[KEYBUF_SIZE];
-static int                              keybuf_head=0;
-static int                              keybuf_tail=0;
+#define	KEYBUF_SIZE	256
+static unsigned char	keybuf[KEYBUF_SIZE];
+static int	keybuf_head = 0;
+static int	keybuf_tail = 0;
 
 extern char     start_of_memory __asm__("start");
 
-byte        scantokey[128] = 
-					{ 
+static byte scantokey[128] = { 
 //  0           1       2       3       4       5       6       7 
 //  8           9       A       B       C       D       E       F 
 	0  ,    27,     '1',    '2',    '3',    '4',    '5',    '6', 
@@ -58,10 +56,10 @@ byte        scantokey[128] =
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0,        // 6 
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0, 
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0         // 7 
-					}; 
+}; 
 
-byte        shiftscantokey[128] = 
-					{ 
+#if 0
+static byte shiftscantokey[128] = { 
 //  0           1       2       3       4       5       6       7 
 //  8           9       A       B       C       D       E       F 
 	0  ,    27,     '!',    '@',    '#',    '$',    '%',    '^', 
@@ -80,9 +78,10 @@ byte        shiftscantokey[128] =
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0,        // 6 
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0, 
 	0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0  ,    0         // 7 
-					}; 
+}; 
+#endif
 
-void TrapKey(void)
+static void TrapKey(void)
 {
 	keybuf[keybuf_head] = dos_inportb(0x60);
 	dos_outportb(0x20, 0x20);
@@ -114,11 +113,10 @@ double Sys_FloatTime (void);
 #define LOCKED_FOR_MALLOC (128*1024)    //FIXME: tune
 
 
-int                     end_of_memory;
-qboolean        lockmem, lockunlockmem, unlockmem;
-qboolean		bSkipWinCheck, bSkipLFNCheck; // FS
-static int      win95;
-static int                      minmem;
+int		end_of_memory;
+static qboolean	lockmem, lockunlockmem, unlockmem;
+static qboolean	bSkipWinCheck, bSkipLFNCheck; // FS
+static int	win95;
 
 // FS: Q2 needs it badly
 // FS: See http://www.delorie.com/djgpp/doc/libc/libc_380.html for more information
@@ -128,7 +126,7 @@ static int                      minmem;
    WEIRD SHIT HAPPENS
    DON'T SEND ME BUG REPORTS FROM A SESSION WITH NO LFN DRIVER LOADED!
 */
-void Sys_DetectLFN (void)
+static void Sys_DetectLFN (void)
 {
 	unsigned int fd = _get_volume_info (NULL, 0, 0, NULL);
 
@@ -145,7 +143,7 @@ void Sys_DetectLFN (void)
 	}
 }
 
-qboolean Sys_DetectWinNT (void) // FS: Wisdom from Gisle Vanem
+static qboolean Sys_DetectWinNT (void) // FS: Wisdom from Gisle Vanem
 {
 	if(_get_dos_version(1) == 0x0532)
 	{
@@ -155,7 +153,7 @@ qboolean Sys_DetectWinNT (void) // FS: Wisdom from Gisle Vanem
 	return false;
 }
 
-void Sys_DetectWin95 (void)
+static void Sys_DetectWin95 (void)
 {
 	__dpmi_regs                             r;
 
@@ -175,18 +173,18 @@ void Sys_DetectWin95 (void)
 	}
 }
 
-__dpmi_meminfo	info; // FS: Sigh, moved this here because everyone wants me to free this shit at exit.  Again, I'm pretty sure CWSDPMI is already taking care of this...
-void *dos_getmaxlockedmem(int *size)
+static __dpmi_meminfo	info; // FS: Sigh, moved this here because everyone wants me to free this shit at exit.  Again, I'm pretty sure CWSDPMI is already taking care of this...
+#if 0
+static int	minmem;
+static void *dos_getmaxlockedmem(int *size)
 {
-	__dpmi_free_mem_info    meminfo;
-	int	working_size;
-	void	*working_memory;
-	int	last_locked;
-//	int	extra,  i, j, allocsize;
-	int	i, j, extra, allocsize; // FS: 2GB Fix
+	__dpmi_free_mem_info	meminfo;
+	int		working_size;
+	void		*working_memory;
+	int		last_locked;
+	int		i, j, extra, allocsize;
 	static char	*msg = "Locking data...";
-	// int	m, n;
-	byte	*x;
+	byte		*x;
 	unsigned long	ul; // FS: 2GB Fix
 
 // first lock all the current executing image so the locked count will
@@ -206,28 +204,24 @@ void *dos_getmaxlockedmem(int *size)
 
 	__dpmi_get_free_memory_information(&meminfo);
 
-	if (!win95)             /* Not windows or earlier than Win95 */
+	if (!win95)	/* Not windows or earlier than Win95 */
 	{
 		ul = meminfo.maximum_locked_page_allocation_in_pages * 4096; // FS: 2GB fix
 	}
 	else
 	{
-		ul = meminfo.largest_available_free_block_in_bytes -
-		LEAVE_FOR_CACHE; // FS: 2GB fix
+		ul = meminfo.largest_available_free_block_in_bytes - LEAVE_FOR_CACHE; // FS: 2GB fix
 	}
 
 	if (ul > 0x7fffffff)
-	{
-		ul = 0x7fffffff; /* limit to 2GB */
-	}
-
+		ul = 0x7fffffff;	/* limit to 2GB */
 	working_size = (int) ul;
-	working_size &= ~0xffff;                /* Round down to 64K */
+	working_size &= ~0xffff;	/* Round down to 64K */
 	working_size += 0x10000;
 
 	do
 	{
-		working_size -= 0x10000;                /* Decrease 64K and try again */
+		working_size -= 0x10000;	/* Decrease 64K and try again */
 		working_memory = sbrk(working_size);
 	}
 	while (working_memory == (void *)-1);
@@ -245,7 +239,7 @@ void *dos_getmaxlockedmem(int *size)
 
 	if (!win95)
 	{
-	    info.size = __djgpp_selector_limit + 1 - last_locked;
+		info.size = __djgpp_selector_limit + 1 - last_locked;
 
 		while (info.size > 0 && __dpmi_lock_linear_region(&info))
 		{
@@ -255,7 +249,7 @@ void *dos_getmaxlockedmem(int *size)
 		}
 	}
 	else
-	{                       /* Win95 section */
+	{	/* Win95 section */
 		j = COM_CheckParm("-winmem");
 
 		minmem = MINIMUM_WIN_MEMORY;
@@ -322,7 +316,6 @@ UpdateSbrk:
 		sbrk(info.address-(int)sbrk(0));                // negative adjustment
 	}
 
-
 	if (lockunlockmem)
 	{
 		__dpmi_unlock_linear_region (&info);
@@ -342,9 +335,9 @@ UpdateSbrk:
 // doing that, of course, but there's no reason we shouldn't)
 	x = (byte *)working_memory;
 
-	for (j=0 ; j<4 ; j++) // FS: 2GB Fix
+	for (j = 0; j < 4; j++)
 	{
-		for (i=0 ; i<(working_size - 16 * 0x1000) ; i += 4)
+		for (i = 0; i < (working_size - 16 * 0x1000); i += 4)
 		{
 			sys_checksum += *(int *)&x[i];
 			sys_checksum += *(int *)&x[i + 16 * 0x1000];
@@ -359,22 +352,18 @@ UpdateSbrk:
 
 	return working_memory;
 }
+#endif
 
-/*
-int virtualmemsize;
-byte *virtualmembase;
-*/
-
-int Sys_Get_Physical_Memory(void) // FS: From DJGPP tutorial
+static int Sys_Get_Physical_Memory(void) /* FS: From DJGPP tutorial */
 {
-	_go32_dpmi_meminfo info;
-	_go32_dpmi_get_free_memory_information(&info);
+	_go32_dpmi_meminfo meminfo;
+	_go32_dpmi_get_free_memory_information(&meminfo);
 
-	if (info.available_physical_pages != -1)
+	if (meminfo.available_physical_pages != -1)
 	{
-		return info.available_physical_pages * 4096;
+		return meminfo.available_physical_pages * 4096;
 	}
-	return info.available_memory;
+	return meminfo.available_memory;
 }
 
 void Sys_Memory_Stats_f (void)
@@ -390,9 +379,9 @@ walks the text, data, and bss to make sure it's all paged in so that the
 actual physical memory detected by Sys_GetMemory is correct.
 ================
 */
-void Sys_PageInProgram(void)
+static void Sys_PageInProgram(void)
 {
-	int             i, j;
+	int		i, j;
 
 	end_of_memory = (int)sbrk(0);
 
@@ -436,7 +425,7 @@ void Sys_PageInProgram(void)
 	printf("%lu Virtual Mb available for Q2DOS.\n", (_go32_dpmi_remaining_virtual_memory() / 0x100000) ); // FS: Added
 }
 
-void Sys_SetTextMode (void) // FS: This was used twice, let's make it a little cleaner if we can.
+static void Sys_SetTextMode (void) // FS: This was used twice, let's make it a little cleaner if we can.
 {
 	__dpmi_regs r;
 
@@ -461,8 +450,8 @@ void Sys_Error (char *error, ...)
 	vprintf (error,argptr);
 	va_end (argptr);
 	printf ("\n");
-	
-	__dpmi_free_physical_address_mapping(&info);	
+
+	__dpmi_free_physical_address_mapping(&info);
 	__djgpp_nearptr_disable(); // FS: Everyone else is a master DOS DPMI programmer.  Pretty sure CWSDPMI is already taking care of this...
 
 #if 0
@@ -491,14 +480,14 @@ void Sys_Quit (void)
 
 	Sys_SetTextMode();
 	
-	__dpmi_free_physical_address_mapping(&info);	
+	__dpmi_free_physical_address_mapping(&info);
 	__djgpp_nearptr_disable(); // FS: Everyone else is a master DOS DPMI programmer.  Pretty sure CWSDPMI is already taking care of this...
 
 	exit (0);
 }
 
-void *GetGameAPI (void *import);
 #ifdef GAME_HARD_LINKED
+void *GetGameAPI (void *import);
 void	Sys_UnloadGame (void)
 {
 }
@@ -651,7 +640,14 @@ double Sys_FloatTime (void)
 	return (double) uclock() / (double) UCLOCKS_PER_SEC; // FS: Win9X/Fast PC Fix (QIP)
 }
 
-void Sys_ParseEarlyArgs(int argc, char **argv) // FS: Parse some very specific args before Qcommon_Init
+void Sys_MakeCodeWriteable(void)
+{
+/* MS-DOS is always writeable */
+}
+
+//=============================================================================
+
+static void Sys_ParseEarlyArgs(int argc, char **argv) // FS: Parse some very specific args before Qcommon_Init
 {
 	int i = 0;
 
@@ -661,16 +657,12 @@ void Sys_ParseEarlyArgs(int argc, char **argv) // FS: Parse some very specific a
 		{
 			bSkipWinCheck = true;
 		}
-
 		if(strncasecmp((char*)argv[i] + 1,"skiplfncheck", 12) == 0)
 		{
 			bSkipLFNCheck = true;
 		}
-
 	}
 }
-
-//=============================================================================
 
 int main (int argc, char **argv)
 {
@@ -706,9 +698,4 @@ int main (int argc, char **argv)
 		sys_frame_time = newtime; // FS: Need to update this for input to work properly
 		oldtime = newtime;
 	}
-}
-
-void Sys_MakeCodeWriteable(void)
-{
-// MS-DOS is always writeable
 }

null/sys_null.c
@@ -27,7 +27,6 @@
 
 #include "../qcommon/qcommon.h"
 #include "../client/keys.h"
-#include "errno.h"
 
 #ifdef __DJGPP__
 // FS: TODO FIXME FIXME PLEASE PLEASE.  >> Take DOS_V2.C from Quake1 and SYS_DOS.C from Quake1.  Clean up this crap :S

osx/sys_null.c
@@ -12,7 +12,6 @@
 
 #include "../qcommon/qcommon.h"
 #include "../client/keys.h"
-#include "errno.h"
 
 static qboolean CompareAttributes(char *path, char *name,
         unsigned musthave, unsigned canthave );
@@ -356,7 +355,7 @@ static  DIR             *fdir;
 
 
 
-char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
+char *Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
 {
         struct dirent *d;
         char *p;
@@ -382,7 +381,7 @@ char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
                 if (!*findpattern || glob_match(findpattern, d->d_name)) {
 //                      if (*findpattern)
 //                              printf("%s matched %s\n", findpattern, d->d_name);
-                        if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+                        if (CompareAttributes(findbase, d->d_name, musthave, canthave)) {
                                 sprintf (findpath, "%s/%s", findbase, d->d_name);
                                 return findpath;
                         }
@@ -391,7 +390,7 @@ char *Sys_FindFirst (char *path, unsigned musthave, unsigned canhave)
         return NULL;
 }
 
-char *Sys_FindNext (unsigned musthave, unsigned canhave)
+char *Sys_FindNext (unsigned musthave, unsigned canthave)
 {
         struct dirent *d;
 
@@ -401,7 +400,7 @@ char *Sys_FindNext (unsigned musthave, unsigned canhave)
                 if (!*findpattern || glob_match(findpattern, d->d_name)) {
 //                      if (*findpattern)
 //                              printf("%s matched %s\n", findpattern, d->d_name);
-                        if (CompareAttributes(findbase, d->d_name, musthave, canhave)) {
+                        if (CompareAttributes(findbase, d->d_name, musthave, canthave)) {
                                 sprintf (findpath, "%s/%s", findbase, d->d_name);
                                 return findpath;
                         }

commit 15809a9f85a2aa14f6b4a37e185bce8eb80f2620

q_shdos.c (CompareAttributes): fix a stupid typo (sems to be introduced by 1fd7688)

dos/q_shdos.c
@@ -108,7 +108,7 @@ static qboolean CompareAttributes(char *path, char *name,
 	return true;
 
 	sprintf (fn, "%s/%s", path, name);
-	if ((attr = _chmod(path, 0)) == -1) /* shouldn't happen */
+	if ((attr = _chmod(fn, 0)) == -1) /* shouldn't happen */
 		return false;
 
 	if ((attr & _A_SUBDIR) && (canthave & SFF_SUBDIR))
@@ -128,9 +128,7 @@ char *Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
 	if (fdir)
 		Sys_Error ("Sys_BeginFind without close");
 
-//	COM_FilePath (path, findbase);
 	strcpy(findbase, path);
-
 	if ((p = strrchr(findbase, '/')) != NULL) {
 		*p = 0;
 		strcpy(findpattern, p + 1);