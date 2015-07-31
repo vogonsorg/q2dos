93c3edd39a6da5f40fb4f1f7f4d001272634ea2e
maraakate <emoaddict15@gmail.com>
2015-07-31 10:49:22 -0400

    typo in gspyi.error message

--- Goa/CEngine/gserverlist.c
+++ Goa/CEngine/gserverlist.c
@@ -449,7 +449,7 @@ GError ServerListLANUpdate(GServerList serverlist, gbool async, int startsearchp
 	GError error;
 
 	if(searchdelta <= 0)
-		gspyi.error("ServerListLANUpdate: searchdetla < 0.");
+		gspyi.error("ServerListLANUpdate: searchdetla <= 0.");
 
 	if (serverlist->state != sl_idle)
 		return GE_BUSY;
