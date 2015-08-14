680674b3606434cc050077f0ce8f5aaab98554a2
maraakate <emoaddict15@gmail.com>
2015-08-14 05:05:55 -0400

    off by one for the strnicmp for Sys_ParseEarlyArgs in sys_dos.c

--- dos/sys_dos.c
+++ dos/sys_dos.c
@@ -451,9 +451,9 @@ static void Sys_ParseEarlyArgs(int argc, char **argv) /* FS: Parse some very spe
 	int i;
 	for (i = 1; i < argc; i++)
 	{
-		if(strnicmp(argv[i],"-skipwincheck",12) == 0)
+		if(strnicmp(argv[i],"-skipwincheck",13) == 0)
 			skipwincheck = true;
-		if(strnicmp(argv[i],"-skiplfncheck",12) == 0)
+		if(strnicmp(argv[i],"-skiplfncheck",13) == 0)
 			skiplfncheck = true;
 	}
 }
