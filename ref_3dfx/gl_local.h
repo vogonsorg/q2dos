761462d22ce8ad33a0b1d864ddb809120469a7aa
maraakate <emoaddict15@gmail.com>
2015-08-18 02:37:38 -0400

    fixed BOPS crud

--- ref_3dfx/gl_local.h
+++ ref_3dfx/gl_local.h
@@ -55,10 +55,13 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	ROLL	2
 
 
+#ifndef __VIDDEF_T
+#define __VIDDEF_T
 typedef struct
 {
 	unsigned		width, height;			// coordinates from main game
 } viddef_t;
+#endif
 
 extern	viddef_t	vid;
 
