c2bcc4859f4e1fd68382a54434f76bc208c426c2
sezero <sezero@users.sourceforge.net>
2015-08-19 14:30:00 +0300

    remove h3 stuff: h5 works for all banshee/v3/v5

--- staticmake/make3dfx.bat
+++ staticmake/make3dfx.bat
@@ -7,10 +7,6 @@ make -f makedxe3_v2.frank
 strip q2fx_v2.exe
 upx -9 q2fx_v2.exe
 
-make -f makedxe3_v3.frank
-strip q2fx_v3.exe
-upx -9 q2fx_v3.exe
-
 make -f makedxe3_v5.frank
 strip q2fx_v5.exe
 upx -9 q2fx_v5.exe
