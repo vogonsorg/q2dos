84f231e284a795fe4f253bb5ac326fc479d49dc3
sezero <sezero@users.sourceforge.net>
2015-08-19 13:51:42 +0300

    remove unnecessary glu.h include

--- ref_3dfx/gl_local.h
+++ ref_3dfx/gl_local.h
@@ -32,7 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 
 #include <GL/gl.h>
-#include <GL/glu.h>
 #include <math.h>
 
 #ifndef GL_COLOR_INDEX8_EXT
