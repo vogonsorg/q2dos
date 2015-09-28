1bbc83a6e89250b2e7176b699cb16e3a6452e489
sezero <sezero@users.sourceforge.net>
2015-09-28 17:32:00 +0300

    remove gl.dxe load disabling hack from qgl_dos.c. ref_gl.dxe is still built dependent on gl.dxe.

--- dos/qgl_dos.c
+++ dos/qgl_dos.c
@@ -32,8 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "glimp_dos.h"
 #ifdef GL_DLSYM
 #include <dlfcn.h>
-#undef GL_DLSYM /* until the issues are resolved.. */
-#define opengl_dxe RTLD_DEFAULT /* until we fix things.. */
 #endif
 
 static FILE *log_fp = NULL;
