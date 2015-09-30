2edeb4dc957f0efee723c045b9d0b54c7e21dc0b
sezero <sezero@users.sourceforge.net>
2015-09-30 11:00:01 +0300

    use a subdir only for sst96, no need for sst1. and do add the subdir to git so it actually doesn't fail. (this still requires a make clean between sst1 and sst96 builds.) (really add the modified files this time...)

--- dos/3rdparty/glide3x.src/mvf.sh
+++ dos/3rdparty/glide3x.src/mvf.sh
@@ -5,5 +5,5 @@ cp sst1/lib/sst96/glide3x.dxe ../lib_dxe/voodoo/sst96
 
 cp cvg/lib/libglide3x.a ../lib/cvg
 cp h5/lib/libglide3x.a ../lib/h5
-cp sst1/lib/sst1/libglide3x.a ../lib/sst1
+cp sst1/lib/libglide3x.a ../lib/sst1
 cp sst1/lib/sst96/libglide3x.a ../lib/sst96
