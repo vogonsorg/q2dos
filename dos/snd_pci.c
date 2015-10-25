abbfc36abd0a87c97d9c4579a532c5001aebca56
sezero <sezero@users.sourceforge.net>
2015-10-25 14:00:00 +0200

    snd_pci.c: added ability to load sndpci.dxe instead of static linking to libau.a (disabled by default for now.) added a precompiled sndpci.dxe.
    Frank: test by building with SNDPCI_DXE=1. if you are happy, then you can
    enable SNDPCI_DXE in the makefiles if you want.

--- dos/snd_pci.c
+++ dos/snd_pci.c
@@ -8,6 +8,57 @@
 
 static au_context *ctx = NULL;
 
+#ifndef SNDPCI_DXE
+#define AU_search_fp AU_search
+#define AU_start_fp AU_start
+#define AU_close_fp AU_close
+#define AU_getinfo_fp AU_getinfo
+#define AU_setrate_fp AU_setrate
+#define AU_setmixer_all_fp AU_setmixer_all
+#define AU_cardbuf_space_fp AU_cardbuf_space
+#else
+static void *sndpci_dxe;
+static au_context * (*AU_search_fp)(unsigned int);
+static void (*AU_start_fp)(au_context *);
+static void (*AU_close_fp)(au_context *);
+static const struct auinfo_s * (*AU_getinfo_fp)(au_context *);
+static void (*AU_setrate_fp)(au_context *, unsigned int *, unsigned int *, unsigned int *);
+static void (*AU_setmixer_all_fp)(au_context *, unsigned int);
+static unsigned int (*AU_cardbuf_space_fp)(au_context *);
+
+static int load_sndpci_dxe(void)
+{
+	if ((sndpci_dxe = Sys_dlopen("sndpci.dxe", 0)) == NULL)
+		goto fail;
+	AU_search_fp = (au_context * (*)(unsigned int)) Sys_dlsym(sndpci_dxe, "_AU_search");
+	AU_start_fp = (void (*)(au_context*)) Sys_dlsym(sndpci_dxe, "_AU_start");
+	AU_close_fp = (void (*)(au_context*)) Sys_dlsym(sndpci_dxe, "_AU_close");
+	AU_getinfo_fp = (const struct auinfo_s * (*)(au_context*)) Sys_dlsym(sndpci_dxe, "_AU_getinfo");
+	AU_setrate_fp = (void (*)(au_context*, unsigned int*, unsigned int*, unsigned int*)) Sys_dlsym(sndpci_dxe, "_AU_setrate");
+	AU_setmixer_all_fp = (void (*)(au_context*, unsigned int)) Sys_dlsym(sndpci_dxe, "_AU_setmixer_all");
+	AU_cardbuf_space_fp = (unsigned int (*)(au_context*)) Sys_dlsym(sndpci_dxe, "_AU_cardbuf_space");
+	if (!AU_search_fp || !AU_start_fp || !AU_close_fp || !AU_getinfo_fp ||
+	    !AU_setrate_fp || !AU_setmixer_all_fp || !AU_cardbuf_space_fp) {
+	fail:	Com_Printf("PCI Audio: failed loading sndpci.dxe\n");
+		return -1;
+	}
+	return 0;
+}
+
+static void close_sndpci_dxe(void)
+{
+	if (sndpci_dxe) Sys_dlclose(sndpci_dxe);
+	sndpci_dxe = NULL;
+	AU_search_fp = NULL;
+	AU_start_fp = NULL;
+	AU_close_fp = NULL;
+	AU_getinfo_fp = NULL;
+	AU_setrate_fp = NULL;
+	AU_setmixer_all_fp = NULL;
+	AU_cardbuf_space_fp = NULL;
+}
+#endif
+
 /*
 ==================
 PCI_Init
@@ -25,7 +76,12 @@ qboolean PCI_Init(void)
 	if (!COM_CheckParm("-sndpci"))
 		return false;
 
-	ctx = AU_search(1);/* 1: stereo speaker output (meaningful only for Intel HDA chips) */
+#ifdef SNDPCI_DXE
+	if (load_sndpci_dxe() < 0)
+		return false;
+#endif
+
+	ctx = AU_search_fp(1);/* 1: stereo speaker output (meaningful only for Intel HDA chips) */
 	if (!ctx)
 	{
 		Com_Printf("PCI Audio: Detection failed.\n");
@@ -37,9 +93,9 @@ qboolean PCI_Init(void)
 	channels = 2;
 	if (s_khz->intValue >= 11025) /* FS */
 		speed = s_khz->intValue;
-	AU_setrate(ctx, &speed, &samplebits, &channels);
+	AU_setrate_fp(ctx, &speed, &samplebits, &channels);
 
-	aui = AU_getinfo(ctx);
+	aui = AU_getinfo_fp(ctx);
 
 	dma.speed = aui->freq_card;
 	dma.samplebits = aui->bits_set;
@@ -53,8 +109,8 @@ qboolean PCI_Init(void)
 
 	Com_Printf("%s\n", aui->infostr);
 
-	AU_setmixer_all(ctx, 80); /* 80% volume */
-	AU_start(ctx); /* also clears dma buffer */
+	AU_setmixer_all_fp(ctx, 80); /* 80% volume */
+	AU_start_fp(ctx); /* also clears dma buffer */
 
 	return true;
 }
@@ -70,7 +126,7 @@ how many sample are required to fill it up.
 */
 int PCI_GetDMAPos(void)
 {
-	dma.samplepos = AU_cardbuf_space(ctx);
+	dma.samplepos = AU_cardbuf_space_fp(ctx);
 	return dma.samplepos;
 }
 
@@ -83,8 +139,11 @@ Stop and close the sound device for exiting
 */
 void PCI_Shutdown(void)
 {
-	AU_close(ctx);
+	AU_close_fp(ctx);
 	ctx = NULL;
+#ifdef SNDPCI_DXE
+	close_sndpci_dxe();
+#endif
 }
 
 #endif /* USE_SNDPCI */
