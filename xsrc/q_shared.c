97fee7c92ade7ab333d9f07832f739686cdf7419
sezero <sezero@users.sourceforge.net>
2016-03-18 11:11:12 +0200

    remove vsnprint() from q_shared and move it under dos, for engine builds against old djgpp versions. minor additions to dxe.c.

--- xsrc/q_shared.c
+++ xsrc/q_shared.c
@@ -17,12 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#ifdef __DJGPP__
-#include <stdio.h>
-#include <limits.h>
-#include <errno.h>
-#include <libc/file.h>
-#endif
+
 #include <ctype.h> /* tolower */
 #include "q_shared.h"
 
@@ -1499,45 +1494,6 @@ void Info_SetValueForKey (char *s, char *key, char *value)
 }
 
 //====================================================================
-
-#ifdef __DJGPP__
-int vsnprintf(char *str, size_t n, const char *fmt, va_list ap)
-{
-  FILE _strbuf;
-  int len;
-
-  /* _cnt is an int in the FILE structure. To prevent wrap-around, we limit
-   * n to between 0 and INT_MAX inclusively. */
-  if (n > INT_MAX)
-  {
-    errno = EFBIG;
-    return -1;
-  }
-
-  memset(&_strbuf, 0, sizeof(_strbuf));
-  _strbuf._flag = _IOWRT | _IOSTRG | _IONTERM;  
-
-  /* If n == 0, just querying how much space is needed. */
-  if (n > 0)
-  {
-    _strbuf._cnt = n - 1;
-    _strbuf._ptr = str;
-  }
-  else
-  {
-    _strbuf._cnt = 0;
-    _strbuf._ptr = NULL;
-  }
-
-  len = _doprnt(fmt, ap, &_strbuf);
-
-  /* Ensure nul termination */
-  if (n > 0)
-    *_strbuf._ptr = 0;
-
-  return len;
-}
-#endif
 
 /* FS: From KMQ2 */
 /*
