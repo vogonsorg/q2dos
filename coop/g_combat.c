f55926bea61861026ecb1e4920be58a5f1dc2731
maraakate <emoaddict15@gmail.com>
2017-01-01 02:30:39 -0500

    fix a rare crash of Killed still happening during a map transition firing off

--- coop/g_combat.c
+++ coop/g_combat.c
@@ -493,7 +493,7 @@ Killed(edict_t *targ, edict_t *inflictor, edict_t *attacker,
 			if (game.gametype != rogue_coop) /* FS: Coop: Rogue handles this elsewhere */
 			{
 				/* medics won't heal monsters that they kill themselves */
-				if (strcmp(attacker->classname, "monster_medic") == 0)
+				if (attacker && attacker->classname && strcmp(attacker->classname, "monster_medic") == 0)
 				{
 					targ->owner = attacker;
 				}
