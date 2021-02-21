c6662ab230e4a1a6b33892d2cf5acd7801a7a21f
maraakate <emoaddict15@gmail.com>
2021-02-20 19:16:56 -0500

    client/console.c:  Con_ToggleConsole_f():  Use Cvar_VariableValueInt.

--- client/console.c
+++ client/console.c
@@ -95,7 +95,7 @@ void Con_ToggleConsole_f (void)
 		M_ForceMenuOff ();
 		cls.key_dest = key_console;	
 
-		if (Cvar_VariableValue ("maxclients") == 1 
+		if (Cvar_VariableValueInt ("maxclients") == 1
 			&& Com_ServerState ())
 			Cvar_Set ("paused", "1");
 	}
